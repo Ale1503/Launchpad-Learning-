a4f50a104e23556bad26d097dbb9fd136b331173
Ale1503 <alejandrovs151@gmail.com>
2020-12-12 15:50:23 -0600

    addition of a new question +new order of the code

--- Trivia.py
+++ Trivia.py
@@ -1,10 +1,21 @@
+def check_keep_playing(game):
+  keep_playing = input("Do you want to try the other game? -> (Yes/No)")
+  if keep_playing == "No":
+    print("Well, it was a pleasure!")
+  if keep_playing == "Yes":
+    if game == 1:
+      game = 2
+      run_game(game)
+    elif game == 2:
+      game = 1
+      run_game(game)
 def run_game(game):
   points = 0
   #First game 
   if game == 1:
     print ("These are all social studies questions! Let's beging! ")
 
-    g1_first = input("What is the name of current president of Costa Rica? ->(Carlos Alvarado/Marito Mortadela/Jose Figueres) ")
+    g1_first = input ("What is the name of current president of Costa Rica? ->(Carlos Alvarado/Marito Mortadela/Jose Figueres) "
     if g1_first == "Carlos Alvarado":
       print("Correct!")
       points = points+1
@@ -18,40 +29,28 @@ def run_game(game):
     else: 
       print("Incorrect, it was in 1824")
 
-    g1_third = input("What was the name of the president who led the Costa Rican army against William Walker? -> (José Figueres/Braulio Carillo/Jose Mora Porras) ")
+    g1_third = input ("What was the name of the president who led the Costa Rican army against William Walker? -> (Jose Figueres/Braulio Carillo/Jose Mora Porras) ")
     if g1_third == "Jose Mora Porras":
       print("Correct!")
       points = points +1 
     else: 
       print("Incorrect, it was Jose Mora Porras")
+
+    g1_fourth = input("Where do you tell poeple to go in Costa Rica when you are mad? ->(Jacó Beach/Fuente de la Hispanidad/El Gollo ")
+    if g1_fourth == "Fuente de la Hispanidad"
+      print("correct!")
+      points = points +1 
+    else: 
+      print("Incorrect, it was Fuente de la Hispanidad")
   
-    print ("You got",points,"/ 3", "points")
+    print ("You got",points,"/ 4", "points")
 
-  #  keep_playing = input("Do you want to try the game 2? -> (Yes/No) ")
-  # if keep_playing == "No":
-    #  print("Well, it was a pleasure!")
-    #if keep_playing == "Yes":
-
+    check_keep_playing(game)
   #Second game 
   elif game == 2: 
     print ("Here we are, let´s beging")
   else: 
     print ("That is not an option, but  ")
-
-
-def check_keep_playing(game):
-  keep_playing = input("Do you want to try the game 2? -> (Yes/No)")
-  if keep_playing == "No":
-    print("Well, it was a pleasure!")
-  if keep_playing == "Yes":
-    if game == 1:
-      game = 2
-      run_game(game)
-    elif game == 2:
-      game = 1
-      run_game(game)
-
-
 #greeting and facts about the player 
 print("Hello! This is a really fun game that you are going to enjoy ")
 name = input("Would you tell me your name? ")
